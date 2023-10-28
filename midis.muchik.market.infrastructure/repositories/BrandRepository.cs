e85188050c48369230913d721f3ff219090b0ecf
Renato De la Rosa <srdelarosab@gmail.com>
2023-10-27 20:32:44 -0500

    Generic Repository

---
+++ midis.muchik.market.infrastructure/repositories/BrandRepository.cs
@@ -0,0 +1,10 @@
+﻿using midis.muchik.market.domain.entities;
+using midis.muchik.market.infrastructure.context;
+
+namespace midis.muchik.market.infrastructure.repositories
+{
+    public class BrandRepository : GenericRepository<Brand>
+    {
+        public BrandRepository(MuchikContext context) : base(context) { }
+    }
+}
